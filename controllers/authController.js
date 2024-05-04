5c2205a912cd5214aad0bfe7e569fc70e025f7b2
Raj Marde <rajmarde615@gmail.com>
2024-05-05 01:16:20 +0530

    Add JWT authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,141 +1,231 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/userModel/usersSchema");
+// const User = require("../models/userModel/usersSchema");
 const bcrypt = require("bcrypt");
+const Admin = require("../models/userModel/admin/AdminSchema");
+const TeacherJrCollege = require("../models/userModel/teacher/teacherTypeModel/TeacherJrCollegeSchema");
+const TeacherCollege = require("../models/userModel/teacher/teacherTypeModel/TeacherCollegeSchema");
+const TeacherSchool = require("../models/userModel/teacher/teacherTypeModel/TeacherSchoolSchema");
+const StudentJrCollege = require("../models/userModel/student/studentTypeModel/StudentJrCollegeSchema");
+const StudentCollege = require("../models/userModel/student/studentTypeModel/StudentCollegeSchema");
+const StudentSchool = require("../models/userModel/student/studentTypeModel/StudentSchoolSchema");
 
-const signup = async (req, res) => {
+// const signup = async (req, res) => {
+//   try {
+//     // Extracting Common user data
+//     const {
+//       firstName,
+//       middleName,
+//       lastName,
+//       phone,
+//       email,
+//       password,
+//       gender,
+//       bloodGroup,
+//     } = req.body;
+
+//     //check for existing user
+//     const existingUser = await User.findOne({ email });
+//     if (existingUser) {
+//       return res
+//         .status(400)
+//         .json({ message: "User already exists with the email" });
+//     }
+//     // Hash the password
+//     const hashedPassword = await bcrypt.hash(password, 10);
+
+//     // Creating new user based on role
+//     let newUser;
+//     switch (role) {
+//       case "Admin":
+//         //Create Admin user
+//         newUser = new Admin({
+//           firstName,
+//           middleName,
+//           lastName,
+//           phone,
+//           email,
+//           password: hashedPassword,
+//           gender,
+//           bloodGroup,
+//         });
+//         break;
+//       case "Student":
+//         const { studentClass, studentBranch, castCategory } = req.body;
+//         newUser = new Student({
+//           firstName,
+//           middleName,
+//           lastName,
+//           phone,
+//           email,
+//           password: hashedPassword,
+//           gender,
+//           bloodGroup,
+//           educationalDetails: { branch: studentBranch, class: studentClass },
+//           castCategory,
+//         });
+//         break;
+//       case "Teacher":
+//         const { subjectSpeciality, teaching } = req.body;
+//         const { teacherBranch, teacherClass, subject } = teaching;
+//         newUser = new Teacher({
+//           firstName,
+//           middleName,
+//           lastName,
+//           phone,
+//           email,
+//           password: hashedPassword,
+//           gender,
+//           bloodGroup,
+//           subjectSpeciality,
+//           teaching: { branch: teacherBranch, class: teacherClass, subject },
+//         });
+//         break;
+//       case "Librarian":
+//         newUser = new Librarian({
+//           firstName,
+//           middleName,
+//           lastName,
+//           phone,
+//           email,
+//           password: hashedPassword,
+//           gender,
+//           bloodGroup,
+//         });
+//         break;
+
+//       default:
+//         break;
+//     }
+//   } catch (e) {
+//     res.send(e);
+//   }
+// };
+
+//Login
+
+const login = async (req, res) => {
   try {
-    // Extracting Common user data
-    const {
-      firstName,
-      middleName,
-      lastName,
-      phone,
-      email,
-      password,
-      gender,
-      bloodGroup,
-    } = req.body;
+    const { email, password, role } = req.body;
 
-    //check for existing user
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res
-        .status(400)
-        .json({ message: "User already exists with the email" });
-    }
-    // Hash the password
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    // Creating new user based on role
-    let newUser;
+    let userModel;
     switch (role) {
-      case "Admin":
-        //Create Admin user
-        newUser = new Admin({
-          firstName,
-          middleName,
-          lastName,
-          phone,
-          email,
-          password: hashedPassword,
-          gender,
-          bloodGroup
-        });
+      case "admin":
+        userModel = Admin;
         break;
-      case "Student":
-        const { studentClass, studentBranch, castCategory } = req.body;
-        newUser = new Student({
-          firstName,
-          middleName,
-          lastName,
-          phone,
-          email,
-          password: hashedPassword,
-          gender,
-          bloodGroup,
-          educationalDetails: { branch: studentBranch, class: studentClass },
-          castCategory,
-        });
+      case "jrCollegeTeacher":
+        userModel = TeacherJrCollege;
         break;
-      case "Teacher":
-        const { subjectSpeciality, teaching } = req.body;
-        const { teacherBranch, teacherClass, subject } = teaching;
-        newUser = new Teacher({
-          firstName,
-          middleName,
-          lastName,
-          phone,
-          email,
-          password: hashedPassword,
-          gender,
-          bloodGroup,
-          subjectSpeciality,
-          teaching: { branch: teacherBranch, class: teacherClass, subject }
-        });
+      case "collegeTeacher":
+        userModel = TeacherCollege;
         break;
-      case "Librarian":
-        newUser = new Librarian({
-          firstName,
-          middleName,
-          lastName,
-          phone,
-          email,
-          password: hashedPassword,
-          gender,
-          bloodGroup,
-        });
+      case "schoolTeacher":
+        userModel = TeacherSchool;
+        break;
+      case "jrCollegeStudent":
+        userModel = StudentJrCollege;
+        break;
+      case "collegeStudent":
+        userModel = StudentCollege;
+        break;
+      case "schoolStudent":
+        userModel = StudentSchool;
         break;
 
       default:
         break;
     }
-  } catch (e) {
-    res.send(e)
-  }
-};
-
-//Login 
-
-const login = async (req, res) => {
-  try {
-    const { email, password } = req.body
 
     //check if the user exist
-    const existingUser = await User.findOne({ email });
+    const existingUser = await userModel.findOne({ email });
 
     if (!existingUser) {
       res.status(401).json({ message: "User does not exists" });
     }
 
     //check if password is correct
-    const isPasswordValid = await bcrypt.compare(password, existingUser.password)
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
 
     if (!isPasswordValid) {
-      return res.status(401).json({ message: "Invalid Credentials" })
+      return res.status(401).json({ message: "Invalid Credentials" });
     }
 
     //Generate JWT Token
 
     let token;
-    token = jwt.sign({
-      email: existingUser.email,
-      firstName: existingUser.firstName,
-      lastName: existingUser.lastName
-    },
-      "My Secret Key",
+    token = jwt.sign(
+      {
+        email: existingUser.email,
+        firstName: existingUser.firstName,
+        lastName: existingUser.lastName,
+        id: existingUser._id,
+        role: existingUser.role
+      },
+      process.env.JWT_SECRET_KEY,
       { expiresIn: "1d" }
     );
 
     //Send Token in response
-    res.status(200).json({ token })
-
+    res.status(200).json({ token });
   } catch (e) {
     console.error("Error during login:", error);
     res.status(500).json({ message: "Internal server error" });
+  } 
+};
+
+//Authenticate function to verify the jwt
+
+const authenticate = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized: Missing token" });
   }
-}
+
+  jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "Unauthorized: Invalid token" });
+    }
+
+    req.user = decoded;
+    next();
+  });
+};
+
+// Middleware to authorize admin access
+const authorizeAdmin = (req, res, next) => {
+  if (req.user.role === "admin") {
+    next();
+  } else {
+    res.status(403).json({ message: "Unauthorized: Access forbidden" });
+  }
+};
+
+// Middleware to authorize teacher access
+const authorizeTeacher = (req, res, next) => {
+  if (req.user.role == "Jr College" || "School" || "College") {
+    next();
+  } else {
+    res.status(403).json({ message: "Unauthorized: Access forbidden" });
+  }
+};
+
+// Middleware to authorize student access
+const authorizeStudent = (req, res, next) => {
+  if (req.user.role === "student") {
+    next();
+  } else {
+    res.status(403).json({ message: "Unauthorized: Access forbidden" });
+  }
+};
 
 module.exports = {
-  signup, 
-  login
-}
+  // signup,
+  login,
+  authorizeAdmin,
+  authorizeStudent,
+  authorizeTeacher,
+  authenticate,
+};
--- routes/Admin/adminRouters.js
+++ routes/Admin/adminRouters.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const addTeacher = require("../../controllers/Admin/addTeacher");
 const addCollegeTeacher = require("../../controllers/Admin/addTeacher");
+const {
+  authenticate,
+  authorizeAdmin,
+} = require("../../controllers/authController");
 const router = express.Router();
 
-
 //Route to add teacher
-router.post('/add-teacher/:roletype', addTeacher)
-
+router.post("/add-teacher/:roletype", authenticate, authorizeAdmin, addTeacher);
 
 module.exports = router;
